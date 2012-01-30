222ac729066137244059381c5821b72c824996fc
Ben Willmore <bdeb@willmore.eu>
2012-01-30 11:02:03 +0000

    bugfix

--- brainware/updatemetadatafile.m
+++ brainware/updatemetadatafile.m
@@ -3,6 +3,7 @@ function updatemetadatafile(filename, newmetadata)
 if exist(filename, 'file')
   l = load(filename);
   metadata = l.metadata;
+  movefile(filename, [filename  '.old.mat']);
 else
   metadata = struct;
 end
@@ -12,7 +13,7 @@ fnames = fieldnames(newmetadata);
 for fname = fnames'
   metadata = setfield(metadata, fname{1}, getfield(newmetadata, fname{1}));
 end
-movefile(filename, [filename  '.old.mat']);
+
 metadata
 fprintf('Saving as %s\n', filename');
 save(filename, 'metadata');
