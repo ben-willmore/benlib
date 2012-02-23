341531110fdd4e6ada430f89e1e2f61602c10030
Ben <ben>
2012-02-23 13:41:07 +0000

    remove keyboard

--- kernels/sepcsdconv.m
+++ kernels/sepcsdconv.m
@@ -21,11 +21,13 @@ k_hd = [kernel.k_hd; kernel.c_d];
 n_d = size(k_hd, 2);
 n_hd = n_h * n_d;
 
-X_fh1t = reshape(X_fht,[n_f, n_h, 1, n_t]);
+%X_fh1t = reshape(X_fht,[n_f, n_h, 1, n_t]);
 
-b_fhdt = X_fh1t .* repmat(k_f, [1 n_h 1 n_t]);
-b_1hdt = sum(b_fhdt, 1);
-b_ht = reshape(b_1hdt(:,:,1,:), [n_h n_t]);
+%b_fhdt = X_fh1t .* repmat(k_f, [1 n_h 1 n_t]);
+%b_1hdt = sum(b_fhdt, 1);
+%b_ht = reshape(b_1hdt(:,:,1,:), [n_h n_t]);
+
+b_ht = squeeze(multiprod(X_fht, k_f, 1));
 
 y_dt = zeros(n_d, n_t);  
 for jj = 1:n_d
