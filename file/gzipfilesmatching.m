a4d518dd98bad0e839dd4006d26197b7b8b7b1d3
Ben Willmore <bdeb@willmore.eu>
2013-05-01 12:56:56 +0100

    fix

--- file/gzipfilesmatching.m
+++ file/gzipfilesmatching.m
@@ -2,6 +2,10 @@ function gzipfilesmatching(pattern)
 
 filenames = getfilesmatching(pattern);
 
+if isempty(filenames)
+  return;
+end
+
 for ii = 1:length(filenames)
   file = filenames{ii};
   gzip(file);
