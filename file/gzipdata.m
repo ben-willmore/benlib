16981f9f3c994af516173a9c56a64078af90b6ec
Ben Willmore <bdeb@willmore.eu>
2013-05-01 13:10:52 +0100

    fix

--- file/gzipdata.m
+++ file/gzipdata.m
@@ -4,5 +4,10 @@ dirnames = getdirsmatching([dirname filesep 'P*']);
 
 for ii = 1:length(dirnames)
   dir = dirnames{ii};
+  try
+ 	getfilesmatching([dir filesep 'raw.f32/*.f32']);
+  catch
+  	return
+  end
   gzipfilesmatching([dir filesep 'raw.f32/*.f32']);
 end
