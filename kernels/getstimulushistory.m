ff64708b9d26b836db0b7d74a7ee4d29ac92f5ce
Ben Willmore <bdeb@willmore.eu>
2013-06-14 17:18:04 +0100

    fixes to getstimulushistory

--- kernels/getstimulushistory.m
+++ kernels/getstimulushistory.m
@@ -1,5 +1,5 @@
 function [X_fht, t] = getstimulushistory(X_ft, dt, bins, dt_new, type)
-% function X_fht = getstimulushistory(X_ft, dt, bins)
+% function [X_fht, t] = getstimulushistory(X_ft, dt, bins, dt_new, type)
 % 
 % Add a history dimension to a stimulus matrix, using non-uniform bins
 % Inputs:
@@ -21,6 +21,10 @@ end
 [n_f, n_t] = size(X_ft);
 n_h = length(bins)-1;
 
+if all(bins==(0:length(bins)-1)*dt)
+  fprintf('Should use tensorize -- much faster\n');
+end
+
 % work out which spectrogram bins will go into each logspaced bin
 idx = round(bins/dt);
 
@@ -34,8 +38,10 @@ for t_idx = 1:n_t
   	  if strcmp(type, 'sum')
 	    X_fht(:, n_h+1-h_idx, t_idx) = sum(X_ft(:, max(mn,1):mx), 2);
 	  elseif strcmp(type, 'mean')
-	    X_fht(:, n_h+1-h_idx, t_idx) = sum(X_ft(:, max(mn,1):mx), 2);
-      else
+      s = sum(X_ft(:, max(mn,1):mx), 2);
+	    X_fht(:, n_h+1-h_idx, t_idx) = s/length(max(mn,1):mx)
+      %X_fht(:, n_h+1-h_idx, t_idx) = mean(X_ft(:, max(mn,1):mx), 2);
+    else
       	error('unknown type -- should be mean or sum')
       end
 	end
@@ -44,9 +50,12 @@ end
 
 if mod(dt_new, dt)~=0
   error('getstimulushistory only implemented for dt_new an integer multiple of dt');
-elseif dt_new~=dt
+else
+  t_old = ((1:size(X_fht, 3))-1)*dt;
   step = dt_new/dt;
-  X_fht = X_fht(:, :, 1:step:end);
-  t = (1:size(X_fht, 3))-1;
-  t = t*dt;
+  start = round(step/2);
+  idx = start:step:size(X_fht, 3);
+
+  X_fht = X_fht(:, :, idx);
+  t = t_old(idx)/1000;
 end
